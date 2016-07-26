debf1c9bb6963b21d737ad03aa70185d045714fd
oulily <ou.lily890@gmail.com>
2016-07-26 16:01:59 -0700

    revised website

---
+++ Week_5/My website/scripts.js
@@ -0,0 +1,22 @@
+// window.onload = function() {
+// 	$(document).on('click', changePhoto);
+// 	image.click(changePhoto);
+// }
+
+// function changePhoto(newurl) {
+// 	var image = $(".profile").children()[0];
+// 	image.src = newurl;
+// }
+
+function myFunction() {
+	document.getElementById("myDropDown").classList.toggle("show");
+}
+
+// document.addEventListener(load) = function() {
+// 	var div = document.getElementByClassName('profile')[0];
+// 	document.addEventListener('click', changePhoto);
+// }
+
+// function changePhoto(newurl) {
+// 	document.getElementByClassName('profile')[0].children[0].src = newurl;
+// }
