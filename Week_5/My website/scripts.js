807542b8ad1a0f647cced4221fc824fb8f891d52
oulily <ou.lily890@gmail.com>
2016-07-27 15:55:11 -0700

    revised website and added bootstrap

--- Week_5/My website/scripts.js
+++ Week_5/My website/scripts.js
@@ -12,19 +12,34 @@ function myFunction() {
 	document.getElementById("myDropDown").classList.toggle("show");
 }
 
-window.onload= function() {
+window.onload = function() {
+	$('.dropdown').on('click', resetDisplay);
+}
+
+function resetDisplay(event){
 	$(".allProjects").hide();
 	var dropItem = event.target.innerHTML;
-	if(dropItem == "Projects") {
-		$("#projects").show();
+	if(dropItem == "Arduino") {
+		$("#Project1").show();
 	}
-	if(dropItem == "Hobbies") {
-		$("#hobbies").show();
+	if(dropItem == "Runner Game") {
+		$("#Project2").show();
 	}
-	if(dropItem == "Fun w/Fam") {
-		$("#funfam").show();
+	if(dropItem == "Scratch") {
+		$("#Project3").show();
 	}
 }
+
+// function resetDisplay(event){
+// 	$(".myHobbies").hide();
+// 	var dropItem = event.target.innerHTML;
+// 	if(dropItem == "Dance") {
+// 		$("#Project1").show();
+// 	}
+// 	if(dropItem == "Music") {
+// 		$("#Project2").show();
+// }
+// list.each(function) performs function for each item on the list
 // document.addEventListener(load) = function() {
 // 	var div = document.getElementByClassName('profile')[0];
 // 	document.addEventListener('click', changePhoto);
@@ -32,4 +47,14 @@ window.onload= function() {
 
 // function changePhoto(newurl) {
 // 	document.getElementByClassName('profile')[0].children[0].src = newurl;
+// }
+
+//make pictures bigger by hovering over it
+// $("div").hover(function() {
+// 	this.animate({
+// 		left: '250px',
+// 		opacity: '0.5',
+// 		height: '150px',
+// 		width: '150px',
+// 	})
 // }
